f41b6e18c9af538e25cb96abf475efa73e65f7ff
abderrahmane <macbook@MacBook-Pro-de-macbook-2.local>
2023-10-09 18:52:15 +0100

    enhance

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 import Feed from '@components/Feed'
+import {Roboto} from 'next/font/google'
+
+
+const robotoRegular = Roboto({
+  subsets:["latin"],  
+  weight:['300','400','900','500','700']
+})
 
 const Home = () => {
   return (
-    <Feed />
+    <main className={robotoRegular.className}>
+      <Feed />
+    </main>
   )
 }
 
